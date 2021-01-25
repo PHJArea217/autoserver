68dd6d9ad566cb5df2c92c24c4e49f754eaaf83b
Peter H. Jin <peter@peterjin.org>
2021-01-24 20:17:44 -0600

    More updates

--- iso-build/build.sh
+++ iso-build/build.sh
@@ -2,6 +2,7 @@
 set -eu
 mkdir -p output/autosvr
 cp ../linux-builder/build_root/arch/x86/boot/bzImage output/vmlinuz
+xz -ce ../linux-builder/build_root/System.map > output/sysmap.xz
 cp ../initramfs-builder/initrd.gz output/
 cp /usr/lib/ISOLINUX/isolinux.bin output/
 cp /usr/lib/syslinux/modules/bios/ldlinux.c32 output/
--- rootfs-builder/build-rootfs.sh
+++ rootfs-builder/build-rootfs.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 unshare -r -m --propagation=slave sh <<\EOF
+set -eu
 mount -t tmpfs -o mode=0700 none /tmp
 M_TMPDIR="$(mktemp -d)"
 chmod 755 "$M_TMPDIR"
 bsdtar -xC "$M_TMPDIR" --no-acls --no-same-owner --no-same-permissions --no-xattrs --chroot -f "rootfs.tar.gz"
 (
+set -eu
 cd "$M_TMPDIR"
 rm -f etc/resolv.conf etc/hosts etc/hostname etc/inittab
 : > etc/resolv.conf
@@ -16,6 +18,6 @@ rm -rf usr/lib/modules usr/src/linux-headers-*
 ln -s /__autoserver__/kernel_modules/modules usr/lib/modules
 )
 # bsdtar -xC "$M_TMPDIR" --no-acls --no-same-owner --no-same-permissions --no-xattrs --chroot -f "../linux-builder/build_root/linux-output.tar.gz" lib/modules
-mv "$M_TMPDIR/lib/modules" "$M_TMPDIR/usr/lib/"
-mksquashfs "$M_TMPDIR" system.img -noappend -comp gzip -b 1048576
+# mv "$M_TMPDIR/lib/modules" "$M_TMPDIR/usr/lib/"
+mksquashfs "$M_TMPDIR" system.img -noappend -comp xz -b 1048576 -Xbcj x86 -Xdict-size 100%
 EOF
