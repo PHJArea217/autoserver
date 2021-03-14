c96b198bbdfb9328b54a140b07988ca2f12d5f66
Peter H. Jin <peter@peterjin.org>
2021-03-14 11:54:58 -0500

    Change to launch ld-linux directly

--- busybox-builder/make-busybox.sh
+++ busybox-builder/make-busybox.sh
@@ -9,7 +9,7 @@ mkdir -p /_libs/i /_libs/b >&2
 cp /sbin/ip /_libs/b/ >&2
 cp /lib64/ld-linux-x86-64.so.2 /_libs/i/ >&2
 for x in /sbin/ip /output_b/busybox-d; do
-	setpriv --reuid=1000 --regid=1000 --clear-groups env LD_TRACE_LOADED_OBJECTS=1 "$x" | sed -n 's!^\t[^ ]* => /\(usr[^ ]*\|lib[^ ]*\) [()x0-9a-f ]*$!\1!p' | xargs -I{} cp -- {} /_libs/i/ >&2
+	setpriv --reuid=1000 --regid=1000 --clear-groups /lib64/ld-linux-x86-64.so.2 --list "$x" | sed -n 's!^\t[^ ]* => /\(usr[^ ]*\|lib[^ ]*\) [()x0-9a-f ]*$!\1!p' | xargs -I{} cp -- /{} /_libs/i/ >&2
 done
 bsdtar -czf /output.tar.gz /_libs && cat /output.tar.gz
 EOF
