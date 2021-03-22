3582d0eeec38f5eeefb3ab9b39ea9d1c2a27ee8c
Peter H. Jin <peter@peterjin.org>
2021-03-22 14:19:41 -0500

    New busybox builder

---
+++ busybox-builder/build-busybox.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+set -eu
+
+bb_sha512=20f8f5197c5cbc8b244f69d82d6628066296c7306a9736ee1344cb555882854412cf7f264490f9a735251c139b9621004f48e972d06ef2623a3c99278f8e765a
+
+if [ ! -f __busybox.tar.bz2 ]; then
+	wget -O __busybox.tar.bz2.tmp https://www.busybox.net/downloads/busybox-1.33.0.tar.bz2
+	sha512sum __busybox.tar.bz2.tmp | grep -q "^$bb_sha512 "
+	mv __busybox.tar.bz2.tmp __busybox.tar.bz2
+fi
+
+mkdir -p __build_root_r/busybox
+if [ ! -f __build_root_r/_busybox_done ]; then
+	bsdtar -xf __busybox.tar.bz2 -C __build_root_r/busybox --strip-components 1
+fi
+
+touch __build_root_r/_busybox_done
+
+sh -c 'set -eu; cd __build_root_r; [ ! -d container-scripts/.git ] && git clone --no-checkout https://git-ipv6.srv.peterjin.org/_/container-scripts; [ ! -d socketbox/.git ] && git clone --no-checkout https://git-ipv6.srv.peterjin.org/_/socketbox; exit 0'
+
+script -c 'exec unshare -r -m -i -u -p -n --fork --mount-proc --propagation=slave setsid sh' __build.log <<\EOF
+set -eux
+
+hostname autoserver
+ip link set lo up
+mount -t tmpfs -o mode=0755 none /proc/driver
+mkdir /proc/driver/build_root_r
+mount --rbind __build_root_r /proc/driver/build_root_r
+cp build-busybox_s.sh /proc/driver/
+exec 3>__busybox_n.tar.gz.tmp 4>__ctr-scripts.tar.gz.tmp
+cd /proc/driver
+mkdir -p dev/pts dev/shm dev/mqueue etc/alternatives proc root run sys tmp usr var
+mount --rbind /etc/alternatives etc/alternatives
+ln -s usr/bin bin
+ln -s usr/lib lib
+ln -s usr/lib64 lib64
+ln -s usr/sbin sbin
+ln -s pts/ptmx dev/ptmx
+ln -s /proc/self/fd dev/fd
+ln -s fd/0 dev/stdin
+ln -s fd/1 dev/stdout
+ln -s fd/2 dev/stderr
+echo root:x:0:0:root:/root:/bin/bash > etc/passwd
+echo root:x:0: > etc/group
+mount -t mqueue none dev/mqueue
+mount -t devpts -o newinstance,ptmxmode=0666,mode=0600 none dev/pts
+for x in full null random tty urandom zero; do
+	: > "dev/$x"
+	mount --bind "/dev/$x" "dev/$x"
+done
+mount --rbind -r /usr usr
+# mount --rbind /sys sys
+mount -t sysfs none sys
+mount -t proc none proc
+
+export HOME=/root
+pivot_root . .
+umount -l .
+if [ "1" = "${DO_SHELL:-0}" ]; then
+	exec sh -c 'exec /bin/bash </dev/tty'
+	exit 1
+fi
+exec setpriv --bounding-set=-all env - sh -s quick-brown-fox < /build-busybox_s.sh
+EOF
+mv __busybox_n.tar.gz.tmp __busybox_n.tar.gz
+mv __ctr-scripts.tar.gz.tmp __ctr-scripts.tar.gz
---
+++ busybox-builder/build-busybox_s.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+
+set -eux
+
+if [ "$1" = "quick-brown-fox" ]; then
+	:
+else
+	echo >&2 This script was not meant to be run directly.
+	exit 1
+fi
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+[ -r /build_root_r/env ] && . /build_root_r/env
+
+mkdir -p /build_tmp
+
+cd /build_tmp
+
+git clone /build_root_r/container-scripts ctr-scripts_x86
+git clone /build_root_r/socketbox socketbox_x86
+git clone /build_root_r/container-scripts ctr-scripts_arm
+git clone /build_root_r/socketbox socketbox_arm
+
+make -C ctr-scripts_x86/ctrtool -j 4 4>&- 3>&-
+make -C ctr-scripts_x86/bind-anywhere -j 4 4>&- 3>&-
+make -C socketbox_x86 -j 4 4>&- 3>&-
+make -C ctr-scripts_arm/ctrtool CROSS_COMPILE=aarch64-linux-gnu- CC=aarch64-linux-gnu-gcc AR=aarch64-linux-gnu-ar ARCH=aarch64 CFLAGS='-O3 -g -fstack-protector-strong -fstack-clash-protection -D_FORTIFY_SOURCE=2 -fPIC -fPIE' -j 4 4>&- 3>&-
+make -C ctr-scripts_arm/bind-anywhere CC=aarch64-linux-gnu-gcc AR=aarch64-linux-gnu-ar CFLAGS='-O3 -g -fstack-protector-strong -fvisibility=hidden -fstack-clash-protection -D_FORTIFY_SOURCE=2 -fPIC' -j 4 4>&- 3>&-
+make -C socketbox_arm CC=aarch64-linux-gnu-gcc AR=aarch64-linux-gnu-ar CFLAGS='-Wall -O2 -fstack-protector-strong -fvisibility=hidden -fstack-clash-protection -D_FORTIFY_SOURCE=2 -fPIC' -j 4 4>&- 3>&-
+
+mkdir /bin_out_a /bin_out_a/amd64 /bin_out_a/aarch64
+
+strip -o /bin_out_a/amd64/ctrtool ctr-scripts_x86/ctrtool/ctrtool
+strip -o /bin_out_a/amd64/ctrtool-static ctr-scripts_x86/ctrtool/ctrtool-static
+strip -o /bin_out_a/amd64/bind-anywhere.so ctr-scripts_x86/bind-anywhere/bind-anywhere.so
+
+aarch64-linux-gnu-strip -o /bin_out_a/aarch64/ctrtool ctr-scripts_arm/ctrtool/ctrtool
+aarch64-linux-gnu-strip -o /bin_out_a/aarch64/ctrtool-static ctr-scripts_arm/ctrtool/ctrtool-static
+aarch64-linux-gnu-strip -o /bin_out_a/aarch64/bind-anywhere.so ctr-scripts_arm/bind-anywhere/bind-anywhere.so
+
+for x in libsocketbox-preload.so socket-query socketbox socketbox-inetd socketbox-inetd-p socketbox-relay send-receive-fd; do
+	strip -o /bin_out_a/amd64/"$x" socketbox_x86/"$x"
+	aarch64-linux-gnu-strip -o /bin_out_a/aarch64/"$x" socketbox_arm/"$x"
+done
+
+chmod -x /bin_out_a/amd64/*.so /bin_out_a/aarch64/*.so
+
+bsdtar -cz -C /bin_out_a -f - . > /dev/fd/4
+
+exec 4>&-
+
+cd /
+rm -rf /build_tmp /bin_out_a
+mkdir /build_tmp
+
+cp -ar /build_root_r/busybox /build_tmp/busybox-d-amd64
+cp -ar /build_root_r/busybox /build_tmp/busybox-d-aarch64
+cp -ar /build_root_r/busybox /build_tmp/busybox-s-amd64
+cp -ar /build_root_r/busybox /build_tmp/busybox-s-aarch64
+
+cd /build_tmp
+cat > busybox_config_common <<\EOF
+CONFIG_BASH_IS_ASH=y
+CONFIG_BBCONFIG=y
+CONFIG_EXTRA_LDFLAGS="-z relro -z now"
+CONFIG_FEATURE_PREFER_APPLETS=y
+CONFIG_FEATURE_PREFER_IPV4_ADDRESS=n
+CONFIG_FEATURE_SH_EXTRA_QUIET=n
+CONFIG_FEATURE_SH_STANDALONE=y
+CONFIG_FEATURE_UNIX_LOCAL=y
+CONFIG_RFKILL=y
+CONFIG_VERBOSE_RESOLUTION_ERRORS=y
+EOF
+
+cat busybox_config_common - > busybox-d-amd64/.config <<\EOF
+CONFIG_EXTRA_CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fcf-protection=full -fstack-clash-protection"
+CONFIG_PIE=y
+EOF
+cat busybox_config_common - > busybox-s-amd64/.config <<\EOF
+CONFIG_EXTRA_CFLAGS="-static-pie -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fcf-protection=full -fstack-clash-protection"
+EOF
+cat busybox_config_common - > busybox-d-aarch64/.config <<\EOF
+# CONFIG_CROSS_COMPILE="aarch64-linux-gnu-"
+CONFIG_EXTRA_CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fstack-clash-protection"
+CONFIG_PIE=y
+EOF
+cat busybox_config_common - > busybox-s-aarch64/.config <<\EOF
+# CONFIG_CROSS_COMPILE="aarch64-linux-gnu-"
+CONFIG_EXTRA_CFLAGS="-static-pie -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fstack-clash-protection"
+EOF
+
+yes "" | make -C busybox-d-amd64 oldconfig 3>&-
+yes "" | make -C busybox-s-amd64 oldconfig 3>&-
+yes "" | make -C busybox-d-aarch64 CROSS_COMPILE=aarch64-linux-gnu- oldconfig 3>&-
+yes "" | make -C busybox-s-aarch64 CROSS_COMPILE=aarch64-linux-gnu- oldconfig 3>&-
+
+make -C busybox-d-amd64 -j 4 3>&-
+make -C busybox-s-amd64 -j 4 3>&-
+make -C busybox-d-aarch64 CROSS_COMPILE=aarch64-linux-gnu- -j 4 3>&-
+make -C busybox-s-aarch64 CROSS_COMPILE=aarch64-linux-gnu- -j 4 3>&-
+
+mkdir /bin_out_b /bin_out_b/aarch64 /bin_out_b/amd64
+cp busybox-d-amd64/busybox /bin_out_b/amd64/busybox-d
+cp busybox-s-amd64/busybox /bin_out_b/amd64/busybox-s
+cp busybox-d-aarch64/busybox /bin_out_b/aarch64/busybox-d
+cp busybox-s-aarch64/busybox /bin_out_b/aarch64/busybox-s
+
+bsdtar -cz -C /bin_out_b -f - . > /dev/fd/3
