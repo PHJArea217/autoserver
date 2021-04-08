112afa2c3d2611e33f8900f2ee03657932314908
Peter H. Jin <peter@peterjin.org>
2021-04-08 02:11:50 -0500

    Add config sanitizer

--- misc/config_sanitizer.sh
+++ misc/config_sanitizer.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-sed '/^\(# CONFIG_[A-Za-z0-9_]* is not set\|CONFIG_[A-Za-z0-9_]*=\([ymn]\|[0-9]\{,10\}\)\)$/d'
+V=
+if [ "$1" = "inverse" ]; then
+	V=-v
+fi
+grep $V '# CONFIG_[A-Za-z0-9_]* is not set\|CONFIG_[A-Za-z0-9_]*=\([ymn]\|[0-9]\{,10\}\)'
