15e1d93a0c9fe5fc018e1f5745d0fd04dfddfb56
Peter H. Jin <peter@peterjin.org>
2021-04-17 07:50:07 -0500

    Add ARMv7 userland

---
+++ rootfs-builder-arm/build-rootfs.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+unshare -r -m --propagation=slave sh <<\EOF
+set -eu
+mount -t tmpfs -o mode=0700 none /tmp
+M_TMPDIR="$(mktemp -d)"
+chmod 755 "$M_TMPDIR"
+bsdtar -xC "$M_TMPDIR" --no-acls --no-same-owner --no-same-permissions --no-xattrs --chroot -f "rootfs.tar.gz"
+(
+set -eu
+cd "$M_TMPDIR"
+for x in etc usr usr/lib usr/src usr/share usr/share/man usr/share/doc; do
+	[ "directory" = "$(stat -c%F "$x")" ]
+done
+rm -f etc/resolv.conf etc/hosts etc/hostname etc/inittab
+: > etc/resolv.conf
+: > etc/hosts
+: > etc/hostname
+: > etc/inittab
+rm -rf usr/lib/modules usr/src/linux-headers-*
+ln -s /__autoserver__/kernel_modules/modules usr/lib/modules
+find usr/share/doc usr/share/man -type f -name '*.gz' -exec gunzip -- {} +
+)
+# bsdtar -xC "$M_TMPDIR" --no-acls --no-same-owner --no-same-permissions --no-xattrs --chroot -f "../linux-builder/build_root/linux-output.tar.gz" lib/modules
+# mv "$M_TMPDIR/lib/modules" "$M_TMPDIR/usr/lib/"
+mksquashfs "$M_TMPDIR" system.img -noappend -comp xz -b 1048576 -Xbcj arm,armthumb -Xdict-size 100%
+EOF
---
+++ rootfs-builder-arm/make-rootfs.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -eu
+
+cat /usr/bin/qemu-arm-static > tmp/qemu-arm-static
+chmod +x tmp/qemu-arm-static
+cat > tmp/Dockerfile <<\EOF
+
+FROM arm32v7/ubuntu:20.04
+
+COPY qemu-arm-static /usr/bin/
+RUN dpkg-divert --add --divert-to /usr/bin/qemu-arm-static.arm /usr/bin/qemu-arm-static
+RUN apt-get update && apt-get install -y ca-certificates
+RUN sed -i "s,http://\(archive\|security\)\.ubuntu\.com/,https://mirrors.edge.kernel.org/,g" /etc/apt/sources.list && \
+	rm -f /etc/dpkg/dpkg.cfg.d/excludes && apt-get update && apt-get -y --no-install-recommends dist-upgrade
+RUN env DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends acct acl adduser alsa-utils \
+apache2 apcalc apparmor apport apt-utils avahi-daemon avahi-utils bc bind9 bind9utils binfmt-support \
+bluez bolt bridge-utils bsdmainutils bsdutils btrfs-progs busybox-initramfs busybox-static ca-certificates cabextract \
+cdw colord console-setup-linux containerd cpufrequtils crda cron cups cups-browsed cups-bsd cups-client \
+cups-core-drivers cups-daemon cups-filters cups-ipp-utils cups-ppdc cups-server-common curl daemontools dante-server dbus \
+debconf debianutils default-jre-headless dhcping dmidecode dmsetup dns-root-data dnsmasq-base \
+dnsutils docker.io dosfstools dpkg-dev dvd+rw-tools e2fsprogs easy-rsa eatmydata ebtables ethtool extundelete fdisk \
+file fuse gdisk genisoimage geoclue-2.0 geoip-database gettext-base git gitlab-runner gnupg gpg gpgv gpm growisofs hdparm \
+hostapd hplip htop initramfs-tools ipcalc ippusbxd iproute2 iptables iptraf-ng iputils-ping ipv6calc \
+ipxe-qemu isc-dhcp-client isc-dhcp-server iso-codes iucode-tool iw kbd keyboard-configuration klibc-utils kmod kpartx \
+less libarchive-tools libcap2-bin libnss-mdns libnss-myhostname libsox-fmt-all linux-base lm-sensors login lsof \
+ltrace man-db manpages manpages-dev mc mdadm mime-support minicom minidlna mlocate moreutils mplayer msr-tools mtools \
+mysql-client mysql-server ncurses-bin ncurses-term net-tools netbase netcat-openbsd nftables nginx-common nginx-extras \
+ntfs-3g ntp openjdk-13-jre-headless openjdk-11-jre-headless openjdk-8-jre-headless openssh-client openssh-server \
+openssh-sftp-server openssl openvpn p7zip-full parted passwd patch pciutils postfix ppp pptp-linux \
+printer-driver-gutenprint printer-driver-hpcups printer-driver-postscript-hp procps psmisc pulseaudio \
+pulseaudio-utils python python3 qemu qemu-block-extra qemu-system qemu-user qemu-user-static qemu-utils radvd rsyslog \
+rtkit runc screen seabios sharutils socat sox squashfs-tools ssh-import-id strace sudo systemd thermald tmux \
+traceroute tshark u-boot-tools udev udisks2 usb-modeswitch usbmuxd util-linux vim-nox vsftpd wamerican wget \
+wireless-regdb wireless-tools wodim wpasupplicant xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-all xterm xxd xz-utils zsh && \
+rm -f /sbin/init /usr/bin/vidir /etc/ssh/ssh_host_*_key* /etc/bind/rndc.key /etc/ssl/private/ssl-cert-snakeoil.key \
+	/etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ssl-cert-snakeoil.pem /usr/bin/man /sbin/switch_root && \
+	sh -c 'set -Ce; printf %s\\n "#!/bin/sh" :\ \$\{LESS=r\} "export LESS" "exec /static/busybox man "\"\$\@\" \
+	> /usr/bin/man' && chmod +x /usr/bin/man && DEBIAN_FRONTEND=noninteractive dpkg-reconfigure ca-certificates
+
+RUN env DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends linux-firmware build-essential make
+
+RUN env DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends isolinux syslinux syslinux-utils syslinux-common \
+gpsd gpsd-clients pps-tools bird quagga-core quagga-bgpd conntrack unbound wireguard-tools slirp4netns \
+lxc-utils
+
+RUN mv /lib/systemd/system /lib/systemd/system_dist && mkdir /lib/systemd/system
+EOF
+docker run --rm -v /docker-buildout/autoserver-arm:/build-output autoserver-rootfs sh -c 'bsdtar -czf - /etc /usr /var > /build-output/rootfs.tar.gz'
