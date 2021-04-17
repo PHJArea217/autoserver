27685692da107cc56bf83276b225866293098f74
me <peter@peterjin.org>
2021-04-17 13:30:01 +0000

    Remove some more x86-specific tools

--- rootfs-builder-arm/make-rootfs.sh
+++ rootfs-builder-arm/make-rootfs.sh
@@ -22,15 +22,15 @@ debconf debianutils default-jre-headless dhcping dmidecode dmsetup dns-root-data
 dnsutils docker.io dosfstools dpkg-dev dvd+rw-tools e2fsprogs easy-rsa eatmydata ebtables ethtool extundelete fdisk \
 file fuse gdisk genisoimage geoclue-2.0 geoip-database gettext-base git gitlab-runner gnupg gpg gpgv gpm growisofs hdparm \
 hostapd hplip htop initramfs-tools ipcalc ippusbxd iproute2 iptables iptraf-ng iputils-ping ipv6calc \
-ipxe-qemu isc-dhcp-client isc-dhcp-server iso-codes iucode-tool iw kbd keyboard-configuration klibc-utils kmod kpartx \
+ipxe-qemu isc-dhcp-client isc-dhcp-server iso-codes iw kbd keyboard-configuration klibc-utils kmod kpartx \
 less libarchive-tools libcap2-bin libnss-mdns libnss-myhostname libsox-fmt-all linux-base lm-sensors login lsof \
-ltrace man-db manpages manpages-dev mc mdadm mime-support minicom minidlna mlocate moreutils mplayer msr-tools mtools \
+ltrace man-db manpages manpages-dev mc mdadm mime-support minicom minidlna mlocate moreutils mplayer mtools \
 mysql-client mysql-server ncurses-bin ncurses-term net-tools netbase netcat-openbsd nftables nginx-common nginx-extras \
 ntfs-3g ntp openjdk-13-jre-headless openjdk-11-jre-headless openjdk-8-jre-headless openssh-client openssh-server \
 openssh-sftp-server openssl openvpn p7zip-full parted passwd patch pciutils postfix ppp pptp-linux \
 printer-driver-gutenprint printer-driver-hpcups printer-driver-postscript-hp procps psmisc pulseaudio \
 pulseaudio-utils python python3 qemu qemu-block-extra qemu-system qemu-user qemu-user-static qemu-utils radvd rsyslog \
-rtkit runc screen seabios sharutils socat sox squashfs-tools ssh-import-id strace sudo systemd thermald tmux \
+rtkit runc screen seabios sharutils socat sox squashfs-tools ssh-import-id strace sudo systemd tmux \
 traceroute tshark u-boot-tools udev udisks2 usb-modeswitch usbmuxd util-linux vim-nox vsftpd wamerican wget \
 wireless-regdb wireless-tools wodim wpasupplicant xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-all xterm xxd xz-utils zsh && \
 rm -f /sbin/init /usr/bin/vidir /etc/ssh/ssh_host_*_key* /etc/bind/rndc.key /etc/ssl/private/ssl-cert-snakeoil.key \
@@ -40,7 +40,7 @@ rm -f /sbin/init /usr/bin/vidir /etc/ssh/ssh_host_*_key* /etc/bind/rndc.key /etc
 
 RUN env DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends linux-firmware build-essential make
 
-RUN env DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends isolinux syslinux syslinux-utils syslinux-common \
+RUN env DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends \
 gpsd gpsd-clients pps-tools bird quagga-core quagga-bgpd conntrack unbound wireguard-tools slirp4netns \
 lxc-utils
 
