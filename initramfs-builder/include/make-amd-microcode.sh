268fc1bb459d19db67f0783a1b1a553f652a1d4d
Peter H. Jin <peter@peterjin.org>
2021-07-03 10:43:32 -0500

    Use print0 for cpio

--- initramfs-builder/include/make-amd-microcode.sh
+++ initramfs-builder/include/make-amd-microcode.sh
@@ -3,4 +3,4 @@ set -euC
 mkdir -p _out/kernel/x86/microcode
 cat /lib/firmware/amd-ucode/*.bin > _out/kernel/x86/microcode/AuthenticAMD.bin
 cd _out
-find | cpio -o -H newc > ../ucode_amd.img
+find -print0 | cpio -0 -o -H newc > ../ucode_amd.img
