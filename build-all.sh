d86408f2a443f5b4b20725911d27bf3aff95918f
Peter H. Jin <peter@peterjin.org>
2021-07-18 08:07:38 -0500

    Ignore standard input

--- build-all.sh
+++ build-all.sh
@@ -5,6 +5,7 @@ run_unpriv() {
 	setpriv --reuid="$SUDO_UID" --regid="$SUDO_GID" --init-groups "$@"
 }
 
+exec </dev/null
 run_unpriv sh build-busybox-initramfs-linux.sh
 sh extra-container-modules/make-all.sh
 run_unpriv sh -c 'cd rootfs-builder && exec sh build-rootfs.sh'
