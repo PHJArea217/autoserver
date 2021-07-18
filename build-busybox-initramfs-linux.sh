e0f82c0c70253961a2629bc88709b5ae7d1c1b60
Peter H. Jin <peter@peterjin.org>
2021-07-17 21:24:13 -0500

    Add scripts/instructions to build on debian live cd

---
+++ build-busybox-initramfs-linux.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eu
+case "$0" in
+	*/*)
+		cd "${0%/*}"
+		;;
+esac
+sh -c 'cd busybox-builder && exec sh build-busybox.sh' </dev/null
+sh -c 'cd initramfs-builder && exec sh build-initramfs.sh' </dev/null
+sh -c 'cd linux-builder && exec sh build-linux.sh' </dev/null
+rm -rf linux-builder/build_root
