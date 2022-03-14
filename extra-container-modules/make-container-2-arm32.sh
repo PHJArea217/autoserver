4ec7903481883c0e2c9531a4baa0ede247669531
Peter H. Jin <peter@peterjin.org>
2022-03-13 23:05:50 -0500

    Change armhf to arm32

--- extra-container-modules/make-container-2-arm32.sh
+++ extra-container-modules/make-container-2-arm32.sh
@@ -11,5 +11,5 @@ EOF
 docker run --rm -v /_autoserver/_ctr-script-build-output_8/node_js:/build_out --entrypoint= -u root ctr-script8-node /bin/sh -c 'tar c /bin /etc /lib /sbin /usr /var > /build_out/rootfs.tar'
 
 . ./common
-write_system 8 100000 armhf
+write_system 8 100000 arm32
 do_build_output 8 55568
