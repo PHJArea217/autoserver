894c49fb55dbbb019c7443d6097b8354059e4c45
Peter H. Jin <peter@peterjin.org>
2022-09-25 16:56:05 -0400

    Add ejs to cm#2 node_js

--- extra-container-modules/make-container-2-arm32.sh
+++ extra-container-modules/make-container-2-arm32.sh
@@ -6,7 +6,7 @@ mkdir -p /_autoserver
 
 docker build -t ctr-script8-node - <<\EOF
 FROM arm32v7/node:lts-slim
-RUN npm install -g express ip axios
+RUN npm install -g express ip axios ejs
 EOF
 docker run --rm -v /_autoserver/_ctr-script-build-output_8/node_js:/build_out --entrypoint= -u root ctr-script8-node /bin/sh -c 'tar c /bin /etc /lib /sbin /usr /var > /build_out/rootfs.tar'
 
--- extra-container-modules/make-container-2.sh
+++ extra-container-modules/make-container-2.sh
@@ -19,7 +19,7 @@ EOF
 
 docker build -t ctr-script2-node - <<\EOF
 FROM node:lts-slim
-RUN npm install -g express ip axios
+RUN npm install -g express ip axios ejs
 EOF
 docker run --rm -v /_autoserver/_ctr-script-build-output_2/matrix-synapse:/build_out --entrypoint= -u root matrixdotorg/synapse /bin/sh -c 'tar c /bin /conf /etc /lib /lib64 /sbin /start.py /usr /var > /build_out/rootfs.tar'
 docker run --rm -v /_autoserver/_ctr-script-build-output_2/mediawiki:/build_out --entrypoint= -u root ctr-script2-mediawiki /bin/sh -c 'tar c /bin /etc /lib /lib64 /sbin /usr /var > /build_out/rootfs.tar'
